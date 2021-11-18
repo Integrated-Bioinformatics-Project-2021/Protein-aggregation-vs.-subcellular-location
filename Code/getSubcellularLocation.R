7a289f535acab862b532f6aac8197f0158c8d6d3
Kaan Ekiz <30717159+ekaan@users.noreply.github.com>
2021-11-18 17:33:56 +0100

    Functionalities: Max Tango and Residues
    Max Tango Analysis, GK, GK+FL residues and their sides are analyzed with a pie chart

--- Code/getSubcellularLocation.R
+++ Code/getSubcellularLocation.R
@@ -4,13 +4,17 @@
 #BiocManager::install("GenomicAlignments")
 #install.packages("sjmisc")
 #install.packages("hash")
-# install.packages("dplyr")
-# install.packages("Peptides")
+#install.packages("dplyr")
+#install.packages("Peptides")
 library(UniprotR)
 library(sjmisc) # Used for str_contains
 library(hash) # Used to make a disctionary
 library(dplyr) # Used for aggregate (get avg tango score of protein)
 library(Peptides) # Used for charge (get the charge of a peptide sequence) ETC
+library(ggplot2)
+library(dplyr)
+library(ggrepel)
+library(tidyverse)
 
 ## Defining the working directory
 directory = dirname(rstudioapi::getSourceEditorContext()$path) # Should work when data is placed in same folder
@@ -27,8 +31,6 @@ if (! file.exists("Data/complete_data.RData")) { # Only retrieve the data when i
   ## Get all unique protein identifiers
   proteins <- unique(data$Protein)
   proteins
-  
-  
   
   ## Get subcellular location of every protein
   locations = GetSubcellular_location(proteins, directorypath = NULL)
@@ -124,7 +126,7 @@ unique_data <- unique_data[!duplicated(unique_data[,c(1,10)]),]
 by_protein <- aggregate(avgScore ~ Protein, unique_data, mean)
 
 # Get average tango score for each APR protein
-unique_APR_data <- only_APR_data[!duplicated(data[,c(1,10)]),]
+unique_APR_data <- only_APR_data[!duplicated(only_APR_data[,c(1,10)]),]
 by_APR_protein <- aggregate(avgScore ~ Protein, unique_APR_data, mean)
 
 #get average Tango score for each subcellular locations for complete proteins 
@@ -139,6 +141,17 @@ get_average_tango_score_complete_protein <- function(given_protein_list) {
   return (avgScores)
 }
 
+#get average max Tango score for each subcellular locations for complete proteins 
+get_avgmax_tango_score_complete_protein <- function(given_protein_list) {
+  avgScores = c()
+  for (j in 1:nrow(given_protein_list)){
+    if (given_protein_list$wanted_proteins[j] %in% by_protein_max$Protein) {
+      avgScores <- append(avgScores,by_protein_max[which(by_protein_max$Protein %in% given_protein_list$wanted_proteins[j]), "maxProtscore"])
+    }
+    else {avgScores <- append(avgScores,NA)}
+  }
+  return (avgScores)
+}
 
 #get average Tango score for each subcellular locations for APR proteins 
 get_average_tango_score_APR_protein <- function(given_protein_list) {
@@ -179,7 +192,6 @@ colnames(tango_scores_APR_protein)  <- c("Proteins", "Subcellular_location", "Ta
 
 #barplot(tango_scores, xlab = "subcellular location", names.arg = search_terms)
 
-library(ggplot2)
 #Plot complete proteins
 box_tango_sub_complete_proteins <- ggplot(tango_scores_complete_protein, aes(x=Tango_scores, y = Subcellular_location, fill = Secretory)) + 
   geom_boxplot(notch=TRUE) + scale_color_brewer(palette="Dark2")
@@ -202,6 +214,28 @@ box_tango_sec_APR_proteins <- ggplot(tango_scores_APR_protein, aes(x=Tango_score
   geom_boxplot(notch=TRUE) + scale_color_brewer(palette="Dark2")
 box_tango_sec_complete_proteins + theme_minimal() + stat_summary(fun=mean, geom="point", shape=20, size=5, color="red", fill="red")
 
+# ----------- Max Tango Scores for each protein and APR -----------------------
+
+by_protein_max = aggregate(maxProtscore~Protein, data, max)
+scores_protein = data.frame()
+
+for(i in 1:length(search_terms))
+{
+  given_protein_list = get_proteins_with_given_subcellular_location(search_terms[i])
+  scores_protein[nrow(scores_protein)+1:nrow(given_protein_list),1] = given_protein_list
+  
+  newIndex_CP = nrow(scores_protein)+1-nrow(given_protein_list)
+  scores_protein[newIndex_CP:(newIndex_CP+nrow(given_protein_list)-1),2] = rep(search_terms[[i]], nrow(given_protein_list))
+  avgMaxScores_protein = get_avgmax_tango_score_complete_protein(given_protein_list)
+  
+  scores_protein[newIndex_CP:(newIndex_CP+nrow(given_protein_list)-1),3] = avgMaxScores_protein
+  scores_protein[newIndex_CP:(newIndex_CP+nrow(given_protein_list)-1),4] = rep(check_secretory(search_terms[i]), nrow(given_protein_list))
+}
+colnames(scores_protein) <- c("Proteins", "Subcellular_location", "Tango_scores", "Secretory")
+
+box_avgmax_complete_proteins <- ggplot(scores_protein, aes(x=Tango_scores, y = Subcellular_location, fill = Secretory)) + 
+  geom_boxplot(notch=TRUE) + scale_color_brewer(palette="Dark2")
+box_avgmax_complete_proteins + theme_minimal() + stat_summary(fun=mean, geom="point", shape=20, size=5, color="red", fill="red")
 
 # ------------------ peptides ---------------------------
 
@@ -225,10 +259,240 @@ get_charge <- function(peptides_name, ph = 7) {
 }
 
 protein_sequences_APR_peptides = get_charge(APR_peptides)
-
-
 # protein_sequences_GK_peptides = get_charge(GK_peptides)
 # protein_sequences_FR_peptides = get_charge(FR_peptides)
 
+#--------- Pie Plot for the residues ------------
+
+# Only for the GK regions
+GK_residues <- subset(data, APRdef2_tango == 2, select = c("Protein","Residue","APRdef2_tango","Side"))
+GK_FL_residues <- subset(data, APRdef2_tango > 1, select = c("Protein","Residue","APRdef2_tango","Side"))
+#group <- unique(GK_residues$Residue)
+#cts = as.data.frame(table(GK_residues$Residue))
+#colnames(cts) <-  c("Residue","Count")
+#pie = ggplot(cts, aes(x="", y=Count, fill=Residue)) + geom_bar(stat="identity", width=1)
+
+cts_gk <- GK_residues %>% 
+  group_by(Residue) %>% # Variable to be transformed
+  count() %>% 
+  ungroup() %>% 
+  mutate(perc = `n` / sum(`n`)) %>% 
+  arrange(perc) %>%
+  mutate(labels = scales::percent(perc))
+
+cts_gk_side <- GK_residues %>% 
+  group_by(Residue, Side) %>% # Variable to be transformed
+  count() %>% 
+  ungroup() %>% 
+  mutate(perc = `n` / sum(`n`)) %>% 
+  arrange(perc) %>%
+  mutate(labels = scales::percent(perc))
+
+cts_gk_fl <- GK_FL_residues %>% 
+  group_by(Residue) %>% # Variable to be transformed
+  count() %>% 
+  ungroup() %>% 
+  mutate(perc = `n` / sum(`n`)) %>% 
+  arrange(perc) %>%
+  mutate(labels = scales::percent(perc))
+
+# This one is done for all of the proteins, but now we will be working on all the proteins that
+# is belonging to a specific subcellular location
+ggplot(cts_gk, aes(x = "", y = perc, fill = Residue)) +
+  geom_col() +
+  geom_label_repel(aes(label = labels), max.overlaps = 30,size = 4.5, position = position_stack(vjust = 0.5), show.legend = FALSE)+
+  coord_polar(theta = "y")+
+  guides(fill = guide_legend(title = "Residue"))+
+  theme_void()
+  #scale_fill_brewer(palette="PiYG")+
+  #geom_label(aes(label = labels), position = position_stack(vjust = 0.5), show.legend = FALSE)
+
+# Lysine (Lys) --> K
+# Arginine (Arg) --> R
+# Aspartic acid (Asp) --> D
+# Glutamic acid (Glu) --> E
+
+lys_ct = cts_gk_side[cts_gk_side$Residue == "K",]
+arg_ct = cts_gk_side[cts_gk_side$Residue == "R",]
+asp_ct = cts_gk_side[cts_gk_side$Residue == "D",]
+glu_ct = cts_gk_side[cts_gk_side$Residue == "E",]
+
+lys_ct <- lys_ct %>%
+  mutate(perc = `n` / sum(`n`)) %>% 
+  arrange(perc) %>%
+  mutate(labels = scales::percent(perc))
+
+arg_ct <- arg_ct %>%
+  mutate(perc = `n` / sum(`n`)) %>% 
+  arrange(perc) %>%
+  mutate(labels = scales::percent(perc))
+
+asp_ct <- asp_ct %>%
+  mutate(perc = `n` / sum(`n`)) %>% 
+  arrange(perc) %>%
+  mutate(labels = scales::percent(perc))
+
+glu_ct <- glu_ct %>%
+  mutate(perc = `n` / sum(`n`)) %>% 
+  arrange(perc) %>%
+  mutate(labels = scales::percent(perc))
+
+ggplot(lys_ct, aes(x = "", y = perc, fill = Side)) +
+  geom_col() +
+  geom_label_repel(aes(label = labels), max.overlaps = 30,size = 4.5, position = position_stack(vjust = 0.5), show.legend = FALSE)+
+  coord_polar(theta = "y")+
+  guides(fill = guide_legend(title = "Side"))+
+  theme_void()
+
+ggplot(arg_ct, aes(x = "", y = perc, fill = Side)) +
+  geom_col() +
+  geom_label_repel(aes(label = labels), max.overlaps = 30,size = 4.5, position = position_stack(vjust = 0.5), show.legend = FALSE)+
+  coord_polar(theta = "y")+
+  guides(fill = guide_legend(title = "Side"))+
+  theme_void()
+
+ggplot(asp_ct, aes(x = "", y = perc, fill = Side)) +
+  geom_col() +
+  geom_label_repel(aes(label = labels), max.overlaps = 30,size = 4.5, position = position_stack(vjust = 0.5), show.legend = FALSE)+
+  coord_polar(theta = "y")+
+  guides(fill = guide_legend(title = "Side"))+
+  theme_void()
+
+ggplot(glu_ct, aes(x = "", y = perc, fill = Side)) +
+  geom_col() +
+  geom_label_repel(aes(label = labels), max.overlaps = 30,size = 4.5, position = position_stack(vjust = 0.5), show.legend = FALSE)+
+  coord_polar(theta = "y")+
+  guides(fill = guide_legend(title = "Side"))+
+  theme_void()
+
+# For each subcellular location
+total_length = length(search_terms)
+total_length
+
+#Give i any value to see the statistics for that subcellular location
+i = 1
+
+  given_protein_list = get_proteins_with_given_subcellular_location(search_terms[i])
+  
+  # FOR ONLY GK
+  
+  wanted <- GK_residues[which(GK_residues$Protein %in% given_protein_list$wanted_proteins),colnames(GK_residues)]
+  
+  cts_gk_s <- wanted %>% 
+    group_by(Residue) %>% # Variable to be transformed
+    count() %>% 
+    ungroup() %>% 
+    mutate(perc = `n` / sum(`n`)) %>% 
+    arrange(perc) %>%
+    mutate(labels = scales::percent(perc))
+  
+  ggplot(cts_gk_s, aes(x = "", y = perc, fill = Residue)) +
+    geom_col() +
+    geom_label_repel(aes(label = labels), max.overlaps = 30,size = 4.5, position = position_stack(vjust = 0.5), show.legend = FALSE)+
+    coord_polar(theta = "y")+
+    guides(fill = guide_legend(title = "Residue"))+
+    theme_void()
+  
+  cts_gk_side <- wanted %>% 
+    group_by(Residue, Side) %>% # Variable to be transformed
+    count() %>% 
+    ungroup() %>% 
+    mutate(perc = `n` / sum(`n`)) %>% 
+    arrange(perc) %>%
+    mutate(labels = scales::percent(perc))
+  
+    lys_ct = cts_gk_side[cts_gk_side$Residue == "K",]
+    arg_ct = cts_gk_side[cts_gk_side$Residue == "R",]
+    asp_ct = cts_gk_side[cts_gk_side$Residue == "D",]
+    glu_ct = cts_gk_side[cts_gk_side$Residue == "E",]
+    
+    ggplot(lys_ct, aes(x = "", y = perc, fill = Side)) +
+      geom_col() +
+      geom_label_repel(aes(label = labels), max.overlaps = 30,size = 4.5, position = position_stack(vjust = 0.5), show.legend = FALSE)+
+      coord_polar(theta = "y")+
+      guides(fill = guide_legend(title = "Side"))+
+      theme_void()
+    
+    ggplot(arg_ct, aes(x = "", y = perc, fill = Side)) +
+      geom_col() +
+      geom_label_repel(aes(label = labels), max.overlaps = 30,size = 4.5, position = position_stack(vjust = 0.5), show.legend = FALSE)+
+      coord_polar(theta = "y")+
+      guides(fill = guide_legend(title = "Side"))+
+      theme_void()
+    
+    ggplot(asp_ct, aes(x = "", y = perc, fill = Side)) +
+      geom_col() +
+      geom_label_repel(aes(label = labels), max.overlaps = 30,size = 4.5, position = position_stack(vjust = 0.5), show.legend = FALSE)+
+      coord_polar(theta = "y")+
+      guides(fill = guide_legend(title = "Side"))+
+      theme_void()
+    
+    ggplot(glu_ct, aes(x = "", y = perc, fill = Side)) +
+      geom_col() +
+      geom_label_repel(aes(label = labels), max.overlaps = 30,size = 4.5, position = position_stack(vjust = 0.5), show.legend = FALSE)+
+      coord_polar(theta = "y")+
+      guides(fill = guide_legend(title = "Side"))+
+      theme_void()
+    
+    # FOR GK + FR
+    
+    wanted <- GK_FL_residues[which(GK_FL_residues$Protein %in% given_protein_list$wanted_proteins),colnames(GK_FL_residues)]
+    
+    cts_gk_fl_s <- wanted %>% 
+      group_by(Residue) %>% # Variable to be transformed
+      count() %>% 
+      ungroup() %>% 
+      mutate(perc = `n` / sum(`n`)) %>% 
+      arrange(perc) %>%
+      mutate(labels = scales::percent(perc))
+    
+    ggplot(cts_gk_fl_s, aes(x = "", y = perc, fill = Residue)) +
+      geom_col() +
+      geom_label_repel(aes(label = labels), max.overlaps = 30,size = 4.5, position = position_stack(vjust = 0.5), show.legend = FALSE)+
+      coord_polar(theta = "y")+
+      guides(fill = guide_legend(title = "Residue"))+
+      theme_void()
+    
+    cts_gk_fl_side <- wanted %>% 
+      group_by(Residue, Side) %>% # Variable to be transformed
+      count() %>% 
+      ungroup() %>% 
+      mutate(perc = `n` / sum(`n`)) %>% 
+      arrange(perc) %>%
+      mutate(labels = scales::percent(perc))
+    
+    lys_ct = cts_gk_fl_side[cts_gk_fl_side$Residue == "K",]
+    arg_ct = cts_gk_fl_side[cts_gk_fl_side$Residue == "R",]
+    asp_ct = cts_gk_fl_side[cts_gk_fl_side$Residue == "D",]
+    glu_ct = cts_gk_fl_side[cts_gk_fl_side$Residue == "E",]
+
+    ggplot(lys_ct, aes(x = "", y = perc, fill = Side)) +
+      geom_col() +
+      geom_label_repel(aes(label = labels), max.overlaps = 30,size = 4.5, position = position_stack(vjust = 0.5), show.legend = FALSE)+
+      coord_polar(theta = "y")+
+      guides(fill = guide_legend(title = "Side"))+
+      theme_void()
+    
+    ggplot(arg_ct, aes(x = "", y = perc, fill = Side)) +
+      geom_col() +
+      geom_label_repel(aes(label = labels), max.overlaps = 30,size = 4.5, position = position_stack(vjust = 0.5), show.legend = FALSE)+
+      coord_polar(theta = "y")+
+      guides(fill = guide_legend(title = "Side"))+
+      theme_void()
+    
+    ggplot(asp_ct, aes(x = "", y = perc, fill = Side)) +
+      geom_col() +
+      geom_label_repel(aes(label = labels), max.overlaps = 30,size = 4.5, position = position_stack(vjust = 0.5), show.legend = FALSE)+
+      coord_polar(theta = "y")+
+      guides(fill = guide_legend(title = "Side"))+
+      theme_void()
+    
+    ggplot(glu_ct, aes(x = "", y = perc, fill = Side)) +
+      geom_col() +
+      geom_label_repel(aes(label = labels), max.overlaps = 30,size = 4.5, position = position_stack(vjust = 0.5), show.legend = FALSE)+
+      coord_polar(theta = "y")+
+      guides(fill = guide_legend(title = "Side"))+
+      theme_void()
+    
 # Make a histogram
 
