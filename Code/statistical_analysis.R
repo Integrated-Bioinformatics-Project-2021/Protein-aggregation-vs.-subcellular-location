0f96ff37292d56443d11adf93fd68d99ace00ef5
Auke Van Sighem <auke@xki.be>
2021-11-28 10:20:15 +0100

    Bugfix (TODO: check stat line 94-95)

--- Code/statistical_analysis.R
+++ Code/statistical_analysis.R
@@ -1,7 +1,8 @@
 # All functions dealing with significance tests
 
-#install.packages("dgof")
 library(dgof)
+library(ggpubr) # Used for ggline
+
 #SUBCELLULAR LOCATION
 #FUNCTION TO CALCULATE SIGNIFICANCE BETWEEN THE SUBCELLULAR LOCATIONS FOR tango_scores_complete_protein, tango_scores_APR_protein, max_tango_scores
 #OUTPUT IS THE OUTPUT FROM THE TEST, BUT MORE IMPORTANTLY A TABLE WITH FOR EACH RELATION OF SUBCELLULAR LOCATIONS THE SIGNIFICANCE IN P-VALUE
@@ -21,13 +22,13 @@ significance_subcellular_nbAPR <- function(data) {
 #FUNTION TO CALCULATE THE SIGNIFICANCE IN GENERAL FOR tango_scores_complete_protein, tango_scores_APR_protein, max_tango_scores
 get_krustal_test_results <- function(data) {
 	sig_subc <- kruskal.test(Tango_scores ~ Subcellular_location, data =  data)
-	return sig_subc
+	return (sig_subc)
 }
 
 #FUNTION TO CALCULATE THE SIGNIFICANCE IN GENERAL FOR normalized_number_APR_regions
 get_krustal_test_results_nbAPR <- function(data) {
 	sig_subc <- kruskal.test(Nb_APR ~ Subcellular_location, data =  data)
-	return sig_subc
+	return (sig_subc)
 }
 
 
@@ -38,7 +39,10 @@ get_wilcox_test_table <- function(data) {
                      p.adjust.method = "BH")
 	table_sig_subc_com_prot<-wilcox_sig_subc_com_prot$p.value
 
-	write.csv(table_sig_subc_com_prot, file = "C:/Users/wpare/OneDrive/Documenten/Studies 2021-2022/Semester 1/Integrated Bioinformatics Project/table_sig_subc_com_prot.csv")
+	print(table_sig_subc_com_prot)
+	
+	file_path = paste(directory, "/Output/table_sig_subc_com_prot.csv", sep="", collapse = NULL)
+	write.csv(table_sig_subc_com_prot, file = file_path)
 }
 
 #FUNTION TO CALCULATE THE SIGNIFICANCE BETWEEN ALL SUBCELLULAR LOCATIONS FOR normalized_number_APR_regions
@@ -48,7 +52,8 @@ get_wilcox_test_table_nbAPR <- function(data) {
                      p.adjust.method = "BH")
 	table_sig_subc_com_prot<-wilcox_sig_subc_com_prot$p.value
 
-	write.csv(table_sig_subc_com_prot, file = "C:/Users/wpare/OneDrive/Documenten/Studies 2021-2022/Semester 1/Integrated Bioinformatics Project/table_sig_subc_com_prot.csv")
+	file_path = paste(directory, "/Output/table_sig_subc_com_prot.csv", sep="", collapse = NULL)
+	write.csv(table_sig_subc_com_prot, file = file_path)
 }
 
 
@@ -64,8 +69,6 @@ group_by(tango_scores_complete_protein, Subcellular_location) %>%
     IQR = IQR(Tango_scores, na.rm = TRUE)
   )
 
-install.packages("ggpubr")
-library("ggpubr")
 ggline(tango_scores_complete_protein, x = "Subcellular_location", y = "Tango_scores", 
        add = c("mean_se", "jitter"), 
        order = c("Cell membrane", "Mitochondrion", "Nucleus", "Endoplasmic Reticulum", "Golgi apparatus", "Lysosome", "Cytoplasm", "Secreted", "Extracellular space"),
@@ -88,8 +91,8 @@ check_significance_sec <- function(x,y){
 #FUNCTION TO CALCULATE SIGNIFICANCE, returning D-value and p-value 
 #FUNCTION USED FOR tango_scores_complete_protein, tango_scores_APR_protein, max_tango_scores
 significance_secretory <- function(data){
-  x <- data$Tango_scores[data$Secretory==TRUE,]
-  y <- data$Tango_scores[data$Secretory==FALSE,]
+  x <- data$Tango_scores[data$Secretory==TRUE]
+  y <- data$Tango_scores[data$Secretory==FALSE]
   return(check_significance_sec(x,y))
 }
 #FUNCTION USED FOR normalized_number_APR_regions
@@ -101,20 +104,20 @@ significance_secretory_nbAPR <- function(data){
 
 #FUNCTION FOR VISUALISATION OF THE KS-TEST FOR tango_scores_complete_protein, tango_scores_APR_protein, max_tango_scores
 plot_ks_test <- function(data) {
-	sample1 <- data$Tango_scores[data$Secretory == TRUE]
-	sample2 <- data$Tango_scores[data$Secretory == FALSE]
-	group <- c(rep("sample1", length(sample1)), rep("sample2", length(sample2)))
-	data_s1_s2<- data.frame(KSD = c(sample1,sample2), group = group)
+	Secretory <- data$Tango_scores[data$Secretory == TRUE]
+	Non_secretory <- data$Tango_scores[data$Secretory == FALSE]
+	group <- c(rep("Secretory", length(Secretory)), rep("Non_secretory", length(Non_secretory)))
+	data_s1_s2<- data.frame(KSD = c(Secretory,Non_secretory), group = group)
 
-	cdf1 <- ecdf(sample1)
-	cdf2 <- ecdf(sample2)
+	cdf1 <- ecdf(Secretory)
+	cdf2 <- ecdf(Non_secretory)
 
-	minMax <- seq(min(sample1, sample2), max(sample1, sample2), length.out=length(sample1)) 
+	minMax <- seq(min(Secretory, Non_secretory), max(Secretory, Non_secretory), length.out=length(Secretory)) 
 	x0 <- minMax[which( abs(cdf1(minMax) - cdf2(minMax)) == max(abs(cdf1(minMax) - cdf2(minMax))) )] 
 	y0 <- cdf1(x0) 
 	y1 <- cdf2(x0) 
 
-	ggplot(data_s1_s2, aes(x = KSD, group = group, color = group)) + stat_ecdf(size=1) + theme_bw(base_size = 28) + theme(legend.position ="top") + xlab("Sample") + ylab("ECDF") + geom_segment(aes(x = x0[1], y = y0[1], xend = x0[1], yend = y1[1]), linetype = "dashed", color = "red") + geom_point(aes(x = x0[1] , y= y0[1]), color="red", size=8) + geom_point(aes(x = x0[1] , y= y1[1]), color="red", size=8) + ggtitle("K-S Test: Sample 1 / Sample 2") + theme(legend.title=element_blank())
+	ggplot(data_s1_s2, aes(x = KSD, group = group, color = group)) + stat_ecdf(size=1) + theme_bw(base_size = 28) + theme(legend.position ="top") + xlab("Group") + ylab("ECDF") + geom_segment(aes(x = x0[1], y = y0[1], xend = x0[1], yend = y1[1]), linetype = "dashed", color = "red") + geom_point(aes(x = x0[1] , y= y0[1]), color="red", size=8) + geom_point(aes(x = x0[1] , y= y1[1]), color="red", size=8) + ggtitle("K-S Test:\nSecretory / Non_secretory") + theme(legend.title=element_blank())
 	}
 
 #FUNCTION FOR VISUALISATION OF THE KS-TEST FOR tango_scores_complete_protein, tango_scores_APR_protein, max_tango_scores
