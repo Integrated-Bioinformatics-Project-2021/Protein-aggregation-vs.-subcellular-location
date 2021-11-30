380caf72728da6f659115378880734210a71dbc3
Kaan Ekiz <30717159+ekaan@users.noreply.github.com>
2021-11-30 10:36:53 +0100

    Documentation + Debug

--- Code/statistical_analysis.R
+++ Code/statistical_analysis.R
@@ -15,7 +15,7 @@ significance_subcellular <- function(data) {
 #FUNCTION TO CALCULATE SIGNIFICANCE BETWEEN THE SUBCELLULAR LOCATIONS FOR normalized_number_APR_regions
 #OUTPUT IS THE OUTPUT FROM THE TEST, BUT MORE IMPORTANTLY A TABLE WITH FOR EACH RELATION OF SUBCELLULAR LOCATIONS THE SIGNIFICANCE IN P-VALUE
 significance_subcellular_nbAPR <- function(data) {
-	sig_subc_APR_prot <- kruskal.test(Nb_APRs ~ Subcellular_location, data = data)
+	sig_subc_APR_prot <- kruskal.test(data$Nb_APRs ~ Subcellular_location, data = data)
 	pairwise.wilcox.test(data$Nb_APRs, data$Subcellular_location,
                      p.adjust.method = "BH")
 }
@@ -28,7 +28,7 @@ get_krustal_test_results <- function(data) {
 
 #FUNTION TO CALCULATE THE SIGNIFICANCE IN GENERAL FOR normalized_number_APR_regions
 get_krustal_test_results_nbAPR <- function(data) {
-	sig_subc <- kruskal.test(Nb_APR ~ Subcellular_location, data =  data)
+	sig_subc <- kruskal.test(Nb_APRs ~ Subcellular_location, data =  data)
 	return (sig_subc)
 }
 
@@ -49,7 +49,7 @@ get_wilcox_test_table <- function(data) {
 #FUNCTION TO CALCULATE THE SIGNIFICANCE BETWEEN ALL SUBCELLULAR LOCATIONS FOR normalized_number_APR_regions
 #RETURNS A TABLE WITH ALL THE P-VALUES
 get_wilcox_test_table_nbAPR <- function(data) {
-	wilcox_sig_subc_com_prot<-pairwise.wilcox.test(data$Nb_APR, data$Subcellular_location,
+	wilcox_sig_subc_com_prot<-pairwise.wilcox.test(data$Nb_APRs, data$Subcellular_location,
                      p.adjust.method = "BH")
 	table_sig_subc_com_prot<-wilcox_sig_subc_com_prot$p.value
 	heatmap(as.matrix(table_sig_subc_com_prot), scale="column", Colv = NA, Rowv = NA)
@@ -111,12 +111,12 @@ plot_stat_significance <- function(data){
 }
 
 plot_stat_significance_nbAPR <- function(data){
-  ggplot(data, aes(x=Subcellular_location, y = Tango_scores, fill = Secretory),
+  ggplot(data, aes(x=Subcellular_location, y = Nb_APRs, fill = Secretory),
          color = "supp", palette = "jco",
          add = "jitter") +
     rotate_x_text(angle = 45)+
     geom_violin() + 
-    geom_hline(yintercept = mean(data$Nb_APR), linetype = 2)+
+    geom_hline(yintercept = mean(data$Nb_APRs), linetype = 2)+
     stat_summary(fun=mean, geom="point", shape=20, size=5, color="red", fill="red") +
     stat_compare_means(aes(label = ..p.adj.., hide.ns = TRUE), method = "wilcox.test",  ref.group = ".all.", hide.ns = TRUE)
 }
